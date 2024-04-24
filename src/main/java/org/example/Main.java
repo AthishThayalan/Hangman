66119267f725c86c2606f284222af5dbecda338a
Athish Thayalan <tathish@hotmail.co.uk>
2024-04-24 16:04:38 +0100

    Added word bank and HangmanDisplay

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,4 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-
 }}
---
+++ src/main/java/org/example/WordBank.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class WordBank {
+    private List<String> words = new ArrayList<>();
+    private Random random = new Random();
+
+    public WordBank() {
+        String wordsString = "ant baboon badger bat bear beaver camel cat clam cobra cougar " +
+                "coyote crow deer dog donkey duck eagle ferret fox frog goat " +
+                "goose hawk lion lizard llama mole monkey moose mouse mule newt " +
+                "otter owl panda parrot pigeon python rabbit ram rat raven " +
+                "rhino salmon seal shark sheep skunk sloth snake spider " +
+                "stork swan tiger toad trout turkey turtle weasel whale wolf " +
+                "wombat zebra";
+
+        String[] wordArray = wordsString.split(" ");
+        for (String word : wordArray) {
+            words.add(word);
+        }
+    }
+
+    public void addWord(String word) {
+        words.add(word);
+    }
+
+    public void removeWord(String word) {
+        words.remove(word);
+    }
+
+    public String selectRandomWord() {
+        if (words.isEmpty()) {
+            System.out.println("There are no words currently available in the Word Bank");
+            return null;
+        }
+        int randomIndex = random.nextInt(words.size());
+        return words.get(randomIndex);
+    }
+
+    public int wordCount() {
+        return words.size();
+    }
+
+    public void printAllWords(){
+        for(String word:words){
+            System.out.println(word);
+        }
+
+    }
+}
